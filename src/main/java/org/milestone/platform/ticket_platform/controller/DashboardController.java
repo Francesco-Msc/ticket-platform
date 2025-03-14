28220accadbb8529b37d57798b2e73069527540b
Francesco-Msc <moscafrancesco1998@gmail.com>
2025-03-14 17:23:27 +0100

    added method to see details of each ticket

--- src/main/java/org/milestone/platform/ticket_platform/controller/DashboardController.java
+++ src/main/java/org/milestone/platform/ticket_platform/controller/DashboardController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +19,11 @@ public class DashboardController {
     public String index(Model model){
         model.addAttribute("tickets", ticketService.findAll());
         return "dashboard/index";
+    }
+
+    @GetMapping("/ticket/{id}")
+    public String show(@PathVariable("id") Integer id, Model model){
+        model.addAttribute("tickets", ticketService.getById(id));
+        return "dashboard/show";
     }
 }
