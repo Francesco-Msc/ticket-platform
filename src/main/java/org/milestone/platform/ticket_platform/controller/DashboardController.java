c5f70b5249b4cbc246cdc7b50e9a7261a787b9f9
Francesco-Msc <moscafrancesco1998@gmail.com>
2025-03-14 16:34:01 +0100

    added DashboardController

---
+++ src/main/java/org/milestone/platform/ticket_platform/controller/DashboardController.java
@@ -0,0 +1,22 @@
+package org.milestone.platform.ticket_platform.controller;
+
+import org.milestone.platform.ticket_platform.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dashboard")
+public class DashboardController {
+
+    @Autowired
+    private TicketService ticketService;
+    
+    @GetMapping
+    public String index(Model model){
+        model.addAttribute("tickets", ticketService.findAll());
+        return "dashboard/index";
+    }
+}
