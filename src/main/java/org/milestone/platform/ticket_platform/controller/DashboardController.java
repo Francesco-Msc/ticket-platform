fece7c1bf5508ba66720996032ece949cd3f037d
Francesco-Msc <moscafrancesco1998@gmail.com>
2025-03-14 19:03:12 +0100

    updated dashboard controller

--- src/main/java/org/milestone/platform/ticket_platform/controller/DashboardController.java
+++ src/main/java/org/milestone/platform/ticket_platform/controller/DashboardController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,9 +20,5 @@ public class DashboardController {
         return "dashboard/index";
     }
 
-    @GetMapping("/ticket/{id}")
-    public String show(@PathVariable("id") Integer id, Model model){
-        model.addAttribute("tickets", ticketService.getById(id));
-        return "dashboard/show";
-    }
+    
 }
