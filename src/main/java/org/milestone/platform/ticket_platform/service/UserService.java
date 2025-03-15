17ad34dc482c472eef643c7cf66be0c6f9ccf37d
Francesco-Msc <moscafrancesco1998@gmail.com>
2025-03-15 19:07:44 +0100

    implemented method to get ticket by user id in userService

--- src/main/java/org/milestone/platform/ticket_platform/service/UserService.java
+++ src/main/java/org/milestone/platform/ticket_platform/service/UserService.java
@@ -3,7 +3,9 @@ package org.milestone.platform.ticket_platform.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.milestone.platform.ticket_platform.model.Ticket;
 import org.milestone.platform.ticket_platform.model.User;
+import org.milestone.platform.ticket_platform.repository.TicketRepository;
 import org.milestone.platform.ticket_platform.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,8 +15,15 @@ public class UserService {
     @Autowired
     private UserRepository userRepo;
 
+    @Autowired
+    private TicketRepository ticketRepo;
+
     public List<User> findAll(){
         return userRepo.findAll();
+    }
+
+    public List<Ticket> getTicketsByUserId(Integer userId) {
+        return ticketRepo.findByUserId(userId);
     }
 
     public List<User> availableOperators(List<User> users) {
