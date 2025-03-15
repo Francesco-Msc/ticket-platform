d7223daffd92891442d6ff9debd2ada08b9ca931
Francesco-Msc <moscafrancesco1998@gmail.com>
2025-03-15 16:11:26 +0100

    added method to find noteByTicketId in note repository

--- src/main/java/org/milestone/platform/ticket_platform/repository/NoteRepository.java
+++ src/main/java/org/milestone/platform/ticket_platform/repository/NoteRepository.java
@@ -1,8 +1,10 @@
 package org.milestone.platform.ticket_platform.repository;
 
+import java.util.List;
+
 import org.milestone.platform.ticket_platform.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoteRepository extends JpaRepository<Note, Integer> {
-
+    public List<Note> findByTicketId(Integer ticketId);
 }
