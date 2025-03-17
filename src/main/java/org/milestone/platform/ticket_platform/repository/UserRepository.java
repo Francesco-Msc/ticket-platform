e6d54247d24f9dd21ed46d72004708cbb07caab7
Francesco-Msc <moscafrancesco1998@gmail.com>
2025-03-17 17:30:35 +0100

    added method in userRepository to find user by username

--- src/main/java/org/milestone/platform/ticket_platform/repository/UserRepository.java
+++ src/main/java/org/milestone/platform/ticket_platform/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package org.milestone.platform.ticket_platform.repository;
 
+import java.util.Optional;
+
 import org.milestone.platform.ticket_platform.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String name);
     
 }
